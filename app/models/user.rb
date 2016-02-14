5c6fb9f1f8260256c9549b5d6c23191e743d7db8
deejkelms <dkelmanson@gmail.com>
2016-02-14 15:55:50 -0500

    some login changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   class << self
   # Returns the hash digest of the given string.
-   def digest(string)
+   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
@@ -23,10 +23,10 @@ validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   end
 
   # Remembers a user in the database for use in persistent sessions.
-  def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
-  end
+  # def remember
+  #   self.remember_token = User.new_token
+  #   update_attribute(:remember_digest, User.digest(remember_token))
+  # end
 
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
@@ -35,8 +35,8 @@ validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   end
 
   # Forgets a user.
-  def forget
-    update_attribute(:remember_digest, nil)
-  end
+  # def forget
+  #   update_attribute(:remember_digest, nil)
+  # end
 end
 end
